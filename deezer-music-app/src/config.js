1022658c5b2b41f5c18ef732c8e92c57f6e78646
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2021-11-09 13:16:45 +0100

    First app spotify

---
+++ deezer-music-app/src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "941d9016ded64be99e363c562fd27ab6";
+export const redirectUri = "http://localhost:3000/redirect";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
