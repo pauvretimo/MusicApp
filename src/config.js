41b81d08ef73d5cce9878bf446d993036f5644d8
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2021-11-12 10:42:36 +0100

    Correction

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = process.env.TOKEN;
-export const redirectUri = "http://151.80.152.87:3000/redirect";
+export const clientId = "941d9016ded64be99e363c562fd27ab6";
+export const redirectUri = "https://cyberlog.dev/redirect";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
